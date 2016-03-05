e30e12491c941f3c4d67298eb7b1179683623e1d
Rich143 <richard.arnold.matthews@gmail.com>
2016-03-05 00:59:44 -0500

    Started scheduling assignments

--- quickstart.py
+++ quickstart.py
@@ -45,6 +45,17 @@ event2 = {
   },
 }
 
+defaultWorkBlock = 1 # 1 hour
+
+#priority: 1-10, 1 is highest
+#estimatedDuration in hours
+assignments = [
+    { 'name' : 'A1', 'dueDate' : datetime.datetime(2016, 3, 7, 10), 'priority' : 1, 'estimatedDuration' : 5, 'timeRemaining': 5},
+    { 'name' : 'A2', 'dueDate' : datetime.datetime(2016, 3, 6, 10), 'priority' : 1, 'estimatedDuration' : 4, 'timeRemaining' : 4}
+]
+
+# global free time list
+freeTime = []
 
 def get_credentials():
     """Gets valid user credentials from storage.
@@ -74,6 +85,38 @@ def get_credentials():
         print('Storing credentials to ' + credential_path)
     return credentials
 
+def getBlockLength(block):
+    return block['end'] - block['start']
+
+def getWorkBlock(length):
+    workBlock = {}
+
+    foundBlock = False
+
+    for idx, freeBlock in enumerate(freeTime):
+        if getBlockLength(freeBlock) >= datetime.timedelta(hours=length):
+            # make work block
+            workBlock['start'] = freeBlock['start']
+            workBlock['end'] = workBlock['start'] + datetime.timedelta(hours=length)
+
+            # update free time
+            if (freeBlock['start'] + datetime.timedelta(hours=length) < freeBlock['end']): # is there time left in block
+                freeTime[idx]['start'] += datetime.timedelta(hours=length)
+            else: # free block filled, remove it
+                freeTime.pop(idx)
+
+            foundBlock = True
+            break
+
+    if (foundBlock == False):
+        print("No free time left")
+        return -1
+    else:
+        return workBlock 
+
+
+
+
 def main():
     """Shows basic usage of the Google Calendar API.
 
@@ -85,8 +128,8 @@ def main():
     service = discovery.build('calendar', 'v3', http=http)
 
     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    max = datetime.datetime.utcnow() + datetime.timedelta(days=5)
-    max_formatted = max.isoformat() + 'Z' 
+    max_event_dateTime = datetime.datetime.utcnow() + datetime.timedelta(days=5) # furthest time in future to fetch events
+    max_formatted = max_event_dateTime.isoformat() + 'Z' 
 
     print('Getting the upcoming events for the next day')
 
@@ -108,6 +151,64 @@ def main():
         #print(start, event['summary'])
 
     events.sort(key=lambda r: r['start']['dateTime'])
+
+    
+
+    for idx, event in enumerate(events):
+        if idx < len(events)-1: #if we don't have the last event
+            freeBlockStart = datetime.datetime.strptime(event['end']['dateTime'],"%Y-%m-%dT%H:%M:%S-05:00") # ****assumes UTC-5******
+            freeBlockEnd = datetime.datetime.strptime(events[idx+1]['start']['dateTime'],"%Y-%m-%dT%H:%M:%S-05:00") # ****assumes UTC-5******
+            freeTime.append({'start' : freeBlockStart, 'end' : freeBlockEnd})
+        else:
+            freeBlockStart = datetime.datetime.strptime(event['start']['dateTime'],"%Y-%m-%dT%H:%M:%S-05:00") # ****assumes UTC-5******
+            #print(endTime)
+            #print(max_event_dateTime)
+            if freeBlockStart < max_event_dateTime:
+                freeTime.append({ 'start' : freeBlockStart, 'end' : max_event_dateTime})
+
+    for freeBlock in freeTime:
+        print(freeBlock['start'].strftime("Start: %H:%M, %b %d, %Y"))
+        print(freeBlock['end'].strftime("End: %H:%M, %b %d, %Y"))
+
+    assignments.sort(key=lambda r: r['priority'], reverse=True) #sort assignment by priority, reverse since 1 is highest priority
+
+    for idx, assignment in enumerate(assignments):
+        duration = assignment['estimatedDuration']
+
+        if (duration < defaultWorkBlock):
+            workBlock = getWorkBlock(duration)
+
+            print("Assignment assigned to time")
+            print(workBlock)
+
+        elif (duration == defaultWorkBlock):
+            workBlock = getWorkBlock(defaultWorkBlock)
+
+            print("Assignment assigned to time")
+            print(workBlock)
+
+        else: # assignment takes more than defaultWorkBlock to complete
+            print(assignments[idx])
+
+            workBlock = getWorkBlock(defaultWorkBlock)
+
+            assignments[idx]['timeRemaining'] -= defaultWorkBlock
+
+            print("Assignment assigned to time")
+            print(workBlock)
+
+
+    for freeBlock in freeTime:
+        print(freeBlock['start'].strftime("Start: %H:%M, %b %d, %Y"))
+        print(freeBlock['end'].strftime("End: %H:%M, %b %d, %Y"))
+
+    """for assignment in assignments:
+        timeRequired = assignments['estimatedDuration']
+
+        if (timeRequired <= defaultWorkBlock):
+            """
+
+
 
     
     #new_event = service.events().insert(calendarId='primary', body=event2).execute()
